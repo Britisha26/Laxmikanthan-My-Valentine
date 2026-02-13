bf8d3c59949bf9253c2dcf04bf49485033d5a385
Tisha <114639906+Britisha26@users.noreply.github.com>
2026-02-14 04:54:55 +0530

    Changed vercel

--- middleware.js
+++ middleware.js
@@ -1,33 +1,28 @@
-import { NextResponse } from 'next/server'
+export default function middleware(request) {
 
-export function middleware(request) {
+  const authHeader = request.headers.get('authorization');
 
-  const authHeader = request.headers.get('authorization')
-
-  const correctPassword = 'laxmiandme'
+  const PASSWORD = 'laxmiandme';
 
   if (authHeader) {
 
-    const base64 = authHeader.split(' ')[1]
-    const decoded = atob(base64)
-    const password = decoded.split(':')[1]
+    const base64 = authHeader.split(' ')[1];
+    const decoded = atob(base64);
+    const password = decoded.split(':')[1];
 
-    if (password === correctPassword) {
-
-      return NextResponse.next()
-
+    if (password === PASSWORD) {
+      return;
     }
-
   }
 
   return new Response('Authentication required', {
     status: 401,
     headers: {
       'WWW-Authenticate': 'Basic realm="Private Valentine"'
-    },
-  })
+    }
+  });
 }
 
 export const config = {
   matcher: '/:path*',
-}
+};
